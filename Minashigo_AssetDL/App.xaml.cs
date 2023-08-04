d37df69eeaff54b36d0a6a8d53d342c1d22fca2b
劃破黑夜 <179971b@gmail.com>
2023-08-05 00:01:31 +0800

    Add files via upload

---
+++ Minashigo_AssetDL/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Minashigo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string Root = Environment.CurrentDirectory;
+        public static string Respath = String.Empty;
+        public static int TotalCount = 0;
+        public static int glocount = 0;
+        public static string ServerURL = "https://minasigo-no-shigoto-pd-c-res.orphans-order.com/";
+        public static List<string> log = new List<string>();
+    }
+}
