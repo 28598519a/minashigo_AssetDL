f94c737de4ca1c35eb91489405a91ccb9e69630c
28598519a <179971b@gmail.com>
2024-05-04 22:59:41 +0800

    Add files via upload

---
+++ Minashigo_AssetDL/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Minashigo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 同時下載的線程池上限
+        /// </summary>
+        int pool = 50;
+
+        private async void btn_download_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.InitialDirectory = App.Root;
+            openFileDialog.Filter = "resource.json|*.json";
+            if (!openFileDialog.ShowDialog() == true)
+                return;
+
+            JObject ResList = JObject.Parse(File.ReadAllText(openFileDialog.FileName));
+            JObject fileList = JObject.Parse(ResList["assets"].ToString());
+            string version = ResList["version"].ToString();
+
+            List<Tuple<string, string>> AssetList = new List<Tuple<string, string>>();
+
+            foreach (JProperty jp in fileList.Properties())
+            {
+                string path = jp.Name.ToString();
+                string url = jp.Value.ToString() + Path.GetExtension(path);
+
+                AssetList.Add(new Tuple<string, string>(path, url));
+            }
+
+            App.TotalCount = AssetList.Count;
+
+            if (App.TotalCount > 0)
+            {
+                App.Respath = Path.Combine(App.Root, "Asset");
+                if (!Directory.Exists(App.Respath))
+                    Directory.CreateDirectory(App.Respath);
+
+                int count = 0;
+                List<Task> tasks = new List<Task>();
+                foreach (Tuple<string, string> asset in AssetList)
+                {
+                    string name = asset.Item1;
+                    string url = App.ServerURL + $"{version}/" + asset.Item2;
+                    string path = Path.Combine(App.Respath, name);
+
+                    tasks.Add(DownLoadFile(url, path, cb_isCover.IsChecked == true ? true : false));
+                    count++;
+
+                    // 阻塞線程，等待現有工作完成再給新工作
+                    if ((count % pool).Equals(0) || App.TotalCount == count)
+                    {
+                        // await is better than Task.Wait()
+                        await Task.WhenAll(tasks);
+                        tasks.Clear();
+                    }
+
+                    // 用await將線程讓給UI更新
+                    lb_counter.Content = $"進度 : {count} / {App.TotalCount}";
+                    await Task.Delay(1);
+                }
+
+                if (cb_Debug.IsChecked == true)
+                {
+                    using (StreamWriter outputFile = new StreamWriter("404.log", false))
+                    {
+                        foreach (string s in App.log)
+                            outputFile.WriteLine(s);
+                    }
+                }
+
+                string failmsg = String.Empty;
+                if (App.TotalCount - App.glocount > 0)
+                    failmsg = $"，{App.TotalCount - App.glocount}個檔案失敗";
+
+                System.Windows.MessageBox.Show($"下載完成，共{App.glocount}個檔案{failmsg}", "Finish");
+                lb_counter.Content = String.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 從指定的網址下載檔案
+        /// </summary>
+        public async Task<Task> DownLoadFile(string downPath, string savePath, bool overWrite)
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(savePath)))
+                Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+
+            if (File.Exists(savePath) && overWrite == false)
+                return Task.FromResult(0);
+
+            App.glocount++;
+
+            using (WebClient wc = new WebClient())
+            {
+                try
+                {
+                    // Don't use DownloadFileTaskAsync, if 404 it will create a empty file, use DownloadDataTaskAsync instead.
+                    byte[] data = await wc.DownloadDataTaskAsync(downPath);
+                    File.WriteAllBytes(savePath, data);
+                }
+                catch (Exception ex)
+                {
+                    App.glocount--;
+
+                    if (cb_Debug.IsChecked == true)
+                        App.log.Add(downPath + Environment.NewLine + savePath + Environment.NewLine);
+
+                    // 沒有的資源直接跳過，避免報錯。
+                    //System.Windows.MessageBox.Show(ex.Message.ToString() + Environment.NewLine + downPath + Environment.NewLine + savePath);
+                }
+            }
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,106 @@
+# minashigo_AssetDL
+Use to download ミナシゴノシゴトR (Minashigo) resources [DMM ver.]
+
+Can download most of the files in ミナシゴノシゴトR, exclude files info from story/readStory.
+
+## decrypt resource.json and calc assets url
+You can just use the final json file I provide in release.
+
+Note: Use Google Chrome - F12 - Console to execute it.
+```
+function loadJs(jsUrl){
+    let scriptTag = document.createElement('script');
+    scriptTag.src = jsUrl;
+    document.getElementsByTagName('head')[0].appendChild(scriptTag);
+};
+
+// load CryptoJS
+loadJs('https://cdn.bootcdn.net/ajax/libs/crypto-js/4.1.1/crypto-js.min.js');
+
+function decrypt(t) {
+var e = CryptoJS.SHA256("#mnsg#manifest"), i = CryptoJS.enc.Base64.stringify(e).substr(0, 32), n = {
+iv: "BFA4332ECFDCB3D1DA2633B5AB509094",
+mode: CryptoJS.mode.CTR
+}, o = CryptoJS.AES.decrypt(t, i, n);
+return CryptoJS.enc.Utf8.stringify(o);}
+
+// [Step1] Modify this by yourself
+str = "text in resource.json"
+
+// [Step2] Now, copy the out and paste it back
+out = decrypt(str)
+
+// [Step3] Because CryptoJS problem, search file name contains "%" and modify it, https://onlineutf8tools.com/convert-hexadecimal-to-utf8
+out = "text in resource.json (edited)"
+
+d = function(t) {
+return [ t.substring(0, 2), t.substring(4, 6) ];
+}, h = function(t) {
+return [ t.substring(2, 4), t.substring(6, 8), t.substring(0, 2) ];
+}, p = function(t) {
+return [ t.substring(4, 6), t.substring(0, 2), t.substring(6, 8), t.substring(2, 4) ];
+}, _ = function(t) {
+return [ t.substring(6, 8), t.substring(2, 4), t.substring(4, 6), t.substring(0, 2) ];
+}, f = {
+0: d,
+1: d,
+2: d,
+3: d,
+4: h,
+5: h,
+6: h,
+7: h,
+8: p,
+9: p,
+a: p,
+b: p,
+c: _,
+d: _,
+e: _,
+f: _
+};
+function g(t) {
+if ("." === t[0]) return "";
+var e = t[0];
+return f[e](t).join("/");
+};
+function convertMD5Path(t) {
+var e = CryptoJS.MD5(t).toString(CryptoJS.enc.Hex), i = t.substr(0, t.lastIndexOf(".")), s = g(CryptoJS.MD5(i).toString(CryptoJS.enc.Hex));return e+"/"+s;}
+
+resource = JSON.parse(out)
+
+var all = '{"version":' + JSON.stringify(resource.version) + ',"assets":{';
+for(path in resource.assets){
+    var value = resource.assets[path]
+    if (value.hasOwnProperty("3")){
+        md5 = value["3"].md5
+    }
+    else {
+        md5 = value["0"].md5
+    }
+    all += JSON.stringify(path) + ':"'+ convertMD5Path(path) + "/" + JSON.stringify(md5).replace('"', '') + ","
+}
+all += "}}"
+
+// [Step4] copy the result of all
+```
+
+## decrypt mnsg data
+1. No idea how to getStoryId(), getSessionId(), so I give up.
+2. For 寝室CG files you can get from here [nyaa](https://sukebei.nyaa.si/view/3913196), but no voice files inside.
+```
+function encrypt(t, e) {
+var i = CryptoJS.SHA256(e + "one-deep"), n = i.toString(CryptoJS.enc.Base64).substr(0, 32), o = {
+iv: (i = CryptoJS.SHA256(e.substr(0, 16))).toString(CryptoJS.enc.Base64).substr(0, 16),
+mode: CryptoJS.mode.CTR
+};
+return CryptoJS.AES.encrypt(t, n, o).toString();
+},
+function decrypt(t, e) {
+var i = CryptoJS.SHA256(e + "one-deep"), n = i.toString(CryptoJS.enc.Base64).substr(0, 32), o = {
+iv: (i = CryptoJS.SHA256(e.substr(0, 16))).toString(CryptoJS.enc.Base64).substr(0, 16),
+mode: CryptoJS.mode.CTR
+};
+return CryptoJS.AES.decrypt(t, n, o).toString(CryptoJS.enc.Utf8);
+}
+```
